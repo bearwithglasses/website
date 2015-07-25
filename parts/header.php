0a42efaa145147ce554b81ba36c61feb03ed4d99
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2015-07-25 19:52:53 -0400

    Initial parts added

---
+++ parts/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<HTML>
+<head>
+	<meta charset="utf-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<meta content="" name="description">
+	<meta content="" name="author">
+	
+	<link href="" rel="favicon">
+	
+	<title></title>
+	
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	
+</head>
+<body>
