f0f6ce28a44eba61fc41cc7862f9477e55e40f89
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2015-07-25 20:44:11 -0400

    Linked menu items

--- parts/menu.php
+++ parts/menu.php
@@ -1,16 +1,16 @@
 <div class="menu row">
-	<div class="mainTitle col-lg-4 col-md-4 col-sm-12 col-xs-12">laughingbear</div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">art</div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">blog</div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">shop</div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">contact</div>
+	<div class="mainTitle col-lg-4 col-md-4 col-sm-12 col-xs-12"><a href="/">laughingbear</a></div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="art">art</a></div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="http://laughingbear.tumblr.com" target=_blank>blog</a></div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="http://shop.laughingbear.us" target=_blank>shop</a></div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="contact">contact</a></div>
 </div>
 <div class="menu row">
 	<div class="col-lg-1 col-md-1 hidden-sm hidden-xs"></div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">illustration</div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">design</div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">comics</div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">products</div>
-	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs">projects</div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="illustration">illustration</a></div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="design">design</a></div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="comics">comics</a></div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="products">products</a></div>
+	<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"><a href="projects">projects</a></div>
 	<div class="col-lg-1 col-md-1 hidden-sm hidden-xs"></div>
 </div>
