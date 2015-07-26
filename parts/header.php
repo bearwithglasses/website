d5b2f79e93a4e14aecf1518f5e5801e161fd2e48
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2015-07-25 20:08:19 -0400

    Vary title depending on page loaded

--- parts/header.php
+++ parts/header.php
@@ -1,3 +1,4 @@
+<?php include('parts/includes.php'); ?>
 <!DOCTYPE html>
 
 <HTML>
@@ -6,11 +7,21 @@
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
 	<meta content="width=device-width, initial-scale=1" name="viewport">
 	<meta content="" name="description">
-	<meta content="" name="author">
+	<meta content="Renee P" name="author">
 	
 	<link href="" rel="favicon">
 	
-	<title></title>
+	<title>
+	<?php
+		// Get the name of the script file, not of the included file
+		$name = $includes[basename($_SERVER['SCRIPT_FILENAME'])]['name'];
+		if( $name!='' ){
+			$name .= ' : ';
+		}
+		$name .= 'laughingbear';
+		echo $name;
+	?>
+	</title>
 	
 	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
 	
---
+++ parts/includes.php
@@ -0,0 +1,32 @@
+<?php
+
+$includes = array(
+	
+	'index.php' => array(
+		'name' => ''
+	),
+	'contact.php' => array(
+		'name' => 'contact'
+	),
+	'art.php' => array(
+		'name' => 'art'
+	),
+	'illustration.php' => array(
+		'name' => 'illustration'
+	),
+	'design.php' => array(
+		'name' => 'design'
+	),
+	'comics.php' => array(
+		'name' => 'comics'
+	),
+	'products.php' => array(
+		'name' => 'products'
+	),
+	'projects.php' => array(
+		'name' => 'projects'
+	)
+
+);
+
+?>
